95a1dfffafbd8c6f825e9420742db8032b02b10b
Zach Maddox <zmaddox@jbssolutions.com>
2024-10-06 16:47:36 -0400

    blinkey

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
 use panic_halt as _;
+
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        let _ = row1.set_low();
+        timer.delay_ms(1_000);
+        let _ = row1.set_high();
+        timer.delay_ms(1_000);
+    }
 }
